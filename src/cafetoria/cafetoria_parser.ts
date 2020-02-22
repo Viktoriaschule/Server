711626344b2bdae4446e4ffe5c82068786c2c7fb
jld3103 <jld3103yt@gmail.com>
2020-02-22 14:02:30 +0100

    Fix cafetoria parser

--- src/cafetoria/cafetoria_parser.ts
+++ src/cafetoria/cafetoria_parser.ts
@@ -1,5 +1,5 @@
 import entities from 'entities';
-import { Cafetoria, CafetoriaDay, Menu } from '../utils/interfaces';
+import {Cafetoria, CafetoriaDay, Menu} from '../utils/interfaces';
 
 /**
  * Extracts the cafetoria data
@@ -11,26 +11,30 @@ const extractData = (data: any, saldoIsNull = false): Cafetoria => {
     if (saldoIsNull) {
         saldo = null;
     }
-    const  rawDates = data.querySelectorAll('.MPDatum');
+    const rawDates = data.querySelectorAll('.MPDatum');
     const dates: CafetoriaDay[] = rawDates.map((a: any) => a.childNodes).map((b: any, day: number): CafetoriaDay => {
         return {
-            day: day, 
+            day: day,
             date: b[0].childNodes[0].rawText,
             menus: []
         };
     });
-    const names = data.querySelectorAll('.angebot_text');
-    const prices = data.querySelectorAll('.angebot_preis');
+    const names = data.querySelectorAll('.angebot_text').reverse();
+    const prices = data.querySelectorAll('.angebot_preis').reverse();
     return {
         saldo: saldo,
-        error: undefined, 
+        error: undefined,
         days: dates.map((date: CafetoriaDay, day: number): CafetoriaDay => {
             let menus: Menu[] = [];
+            let extraTime: string = '';
             for (let i = 0; i < 4; i++) {
                 let text = names[day * 4 + i].childNodes.length >= 1 ? names[day * 4 + i].childNodes.map((a: any) => a.rawText).join(' ').replace('  ', ' ') : '';
                 text = entities.decodeHTML(text);
-                let time = '';
-                if (text.includes(' Uhr ')) {
+                let time: string = '';
+                if (text.includes('bis') && text.includes(' Uhr ') && text.includes('abholen')) {
+                    extraTime = text.replace('Menü bitte', '').replace('abholen.', '').replace('.', ':').trim();
+                    continue;
+                } else if (text.includes(' Uhr ')) {
                     time = text.split(' Uhr ')[0];
                     text = text.split(' Uhr ')[1];
                     time = time.replace(/\./g, ':');
@@ -48,13 +52,16 @@ const extractData = (data: any, saldoIsNull = false): Cafetoria => {
                     }
                     time = timeS + (timeE !== '' ? ' - ' + timeE : '') + ' Uhr';
                 }
+                if (time == '' && extraTime != '') {
+                    time = extraTime;
+                }
                 menus.push({
                     name: text,
                     time: time,
                     price: prices[dates.indexOf(date) * 4 + i].childNodes.length == 1 ? parseFloat(prices[dates.indexOf(date) * 4 + i].childNodes[0].rawText.replace('&euro;', '').trim().replace(',', '.')) : 0
                 });
             }
-            date.menus = menus.filter((a: any) => a.name !== '');
+            date.menus = menus.reverse().filter((a: any) => a.name !== '');
             return date;
         })
     };
